47db8d987795e9af5a2536aba8892d7d1dbc6764
htf <837305770@qq.com>
2018-12-17 21:14:16 +0800

    "this is project init"

--- src/main/java/com/baizhi/SpringBootCommonMapper2Application.java
+++ src/main/java/com/baizhi/SpringBootCommonMapper2Application.java
@@ -10,6 +10,7 @@ public class SpringBootCommonMapper2Application {
 
 
     public static void main(String[] args) {
+        System.out.println("2222222222222222222");
         SpringApplication.run(SpringBootCommonMapper2Application.class, args);
     }
 
